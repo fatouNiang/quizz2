863bf6a611908e43a899e9172a0264f814f7802c
Fatou Niang <fatou04.niang@gmail.com>
2020-05-28 08:31:47 +0100

    integration connexion

---
+++ Projet2/pages/connexion.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-sm  part-bleu">
+        <h2 class="title">BIENVENU DANS NOTRE QUIZZ</h2>
+        <!-- <h4>veuillez vous connecter ou vous inscrire au jeux</h4> -->
+        <!-- <h6>vous n'avez pas de compte ?</h6> -->
+        <!-- <button class="btn-white">creer un compte</button> -->
+    </div>
+    <div class="col-sm part-blanche">
+        <h2 class="login">LOGIN</h2>
+        <form>
+            <div class="form-group">
+                <input type="text" class="form-control" id="login" placeholder="login">
+                <div class="img1"><img src="./public/image/icone1.jpg" alt="" class="img1"></div>
+                <small id="login" class="form-text text-muted"></small>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" id="password" placeholder="password">
+                <div class="img1"><img src="./public/image/password.png" alt="" class="img1"></div>
+                <small id="password" class="form-text text-muted"></small>
+            </div>
+            <button type="submit" class="btn btn-primary">CONNEXION</button>
+            <h6>vous n'avez pas de compte ?</h6>
+            <a href="" class="btn-white">creer un compte</a>
+        </form>
+    </div>
+</div>
